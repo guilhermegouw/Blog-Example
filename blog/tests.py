fb4cdeb6231782dffb57fbaaaea905fe23722ee8
guilhermegouw <guilherme.gouw@gmail.com>
2022-04-09 23:15:59 -0300

    implemented post detail view

--- blog/tests.py
+++ blog/tests.py
@@ -1,4 +1,4 @@
-from turtle import title
+from urllib import response
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from .models import Post
@@ -38,7 +38,32 @@ class PostTests(TestCase):
         self.assertEqual(post.get(), self.published_post)
 
 
-class PostListView(TestCase):
+class PostListViewTests(TestCase):
     def test_post_list_view_status_code(self):
         response = self.client.get("/blog/")
+        self.assertEqual(response.status_code, 200)
+
+
+class PostDetailViewTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="testuser", email="testuser@email.com", password="secret"
+        )
+        self.post = Post.objects.create(
+            title="Test Post",
+            slug="test-post",
+            author=self.user,
+            body="Body full of content",
+        )
+        self.published_post = Post.objects.create(
+            title="Another Post",
+            slug="another-post",
+            author=self.user,
+            body="Another body full of content",
+            status="published",
+        )
+
+    def test_post_detail_view_status_code(self):
+        published_post_url = self.published_post.get_absolute_url()
+        response = self.client.get(published_post_url)
         self.assertEqual(response.status_code, 200)
--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
+from django import views
 from django.urls import path
-from .views import post_list
+from .views import post_list, post_detail
 
 app_name = "blog"
 
 urlpatterns = [
     path("", post_list, name="post_list"),
+    path(
+        "<int:year>/<int:month>/<int:day>/<slug:post>/", post_detail, name="post_detail"
+    ),
 ]
